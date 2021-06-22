a5737f01ae0979056ca705a3d38066373631ca0d
Emory <aa774281@gmail.com>
2021-06-22 20:37:33 +0800

    comments paginator implement

--- index.php
+++ index.php
@@ -84,15 +84,26 @@ if (!empty($_SESSION['username'])) {
     <!-- 根據資料庫拿到相對應留言資訊 -->
     <section>
       <?php
+      // 根據 GET 拿到頁數
+      $page = 1;
+      if (!empty($_GET['page'])) {
+        $page = intval($_GET['page']);
+      }
+      // 每一頁有多少筆資料
+      $items_per_page = 10;
+      // 計算出每一頁初始資料的偏移量，就是 (分頁數 - 1) * 每頁有多少筆資料
+      $offset = ($page - 1) * $items_per_page;
       // 為了使修改過後的 nickname 能夠同步到之前的留言
       // 根據 comments 的 username 找出 users 的 nickname
-      $sql = 
-      "SELECT C.id as comment_id, C.created_at as created_at, U.nickname as nickname, U.username as username, C.content as content " .
-      "FROM comments as C LEFT JOIN users as U " .
-      "ON C.username = U.username ".
-      "WHERE C.is_deleted is NULL ".
-      "ORDER BY C.created_at DESC";
+      $sql =
+        "SELECT C.id as comment_id, C.created_at as created_at, U.nickname as nickname, U.username as username, C.content as content " .
+        "FROM comments as C LEFT JOIN users as U " .
+        "ON C.username = U.username " .
+        "WHERE C.is_deleted is NULL " .
+        "ORDER BY C.created_at DESC " .
+        "limit ? offset ?";
       $stmt = $conn->prepare($sql);
+      $stmt->bind_param("ii", $items_per_page, $offset);
       $res = $stmt->execute();
       if (!$res) {
         die($conn->error);
@@ -106,20 +117,49 @@ if (!empty($_SESSION['username'])) {
           <div class="card__body">
             <div class="card__info">
               <span class="card__info__author"><?php echo escape($row['nickname']) ?>
-              (@<?php echo escape($row['username'])?>)</span>
+                (@<?php echo escape($row['username']) ?>)</span>
               <span class="card__info__time"><?php echo escape($row['created_at']) ?></span>
-              <?php if ($row["username"] === $username) {?>
-              <a class="card__info__edit" href="update_comment.php?id=<?php echo escape($row['comment_id']) ?>">編輯留言</a>
-              <a class="card__info__edit" href="handle_delete_comment.php?id=<?php echo escape($row['comment_id']) ?>">刪除留言</a>
-              <?php }?>
+              <?php if ($row["username"] === $username) { ?>
+                <a class="card__info__edit" href="update_comment.php?id=<?php echo escape($row['comment_id']) ?>">編輯留言</a>
+                <a class="card__info__edit" href="handle_delete_comment.php?id=<?php echo escape($row['comment_id']) ?>">刪除留言</a>
+              <?php } ?>
             </div>
             <p class="card__content"><?php echo escape($row['content']) ?></p>
           </div>
         </div>
       <?php } ?>
     </section>
+    <div class="board__hr"></div>
+    <?php
+    $sql =
+      "SELECT COUNT(id) as count FROM comments WHERE is_deleted is NULL";
+    $stmt = $conn->prepare($sql);
+    // $stmt->bind_param("ii", $items_per_page, $offset);
+    $res = $stmt->execute();
+    if (!$res) {
+      die($conn->error);
+    }
+    $res = $stmt->get_result();
+    $row = $res->fetch_assoc();
+    $count = $row['count'];
+    // ceil 回傳是 float 型態，因為之後要跟 int 比較，所以用 intval 轉成 int 型態
+    $total_pages = intval(ceil($count / $items_per_page));
+    ?>
+    <div class="page">
+      <span class="page__info">總共有 <?php echo $count ?> 筆留言，目前分頁為 <?php echo $page ?> / <?php echo $total_pages ?></span>
+      <div class="page__paginator">
+        <?php if ($page !== 1) { ?>
+          <a href="index.php?page=1">第一頁</a>
+          <a href="index.php?page=<?php echo $page - 1 ?>">上一頁</a>
+        <?php } ?>
+        <?php if ($page !== $total_pages) { ?>
+          <a href="index.php?page=<?php echo $page + 1 ?>">下一頁</a>
+          <a href="index.php?page=<?php echo $total_pages ?>">最後一頁</a>
+        <?php } ?>
+      </div>
+    </div>
   </main>
-<script src="index.js"></script>
+  <script src="index.js"></script>
 </body>
 
 </html>
